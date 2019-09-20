ec095981318a675d1a8fdd4513fbffbc1853f809
A.Ciprian <ciprian.anita96@yahoo.com>
2019-09-20 10:57:48 +0300

    Calculate number of red mushrooms for a bigger multiplying factor

--- JuniorMind1/Mushrooms/Mushrooms.cs
+++ JuniorMind1/Mushrooms/Mushrooms.cs
@@ -12,6 +12,13 @@ namespace Mushrooms
             Assert.Equal(5, redMushrooms);
         }
 
+        [Fact]
+        public void NumberOfRedMushroomsWhenMultiplyingFactorHasABiggerPositiveValue()
+        {
+            int redMushrooms = CalculateNumberOfRedMushrooms(15, 2);
+            Assert.Equal(10, redMushrooms);
+        }
+
         int CalculateNumberOfRedMushrooms(int numberOfMushrooms, int xFactorForWhiteMushrooms)
         {
             int whiteMushrooms = numberOfMushrooms / (xFactorForWhiteMushrooms + 1);
