e8038981209c12ba2959fa899b0cb716f6aa3002
A.Ciprian <ciprian.anita96@yahoo.com>
2019-09-20 11:48:39 +0300

    Calculate number of boards for integer area parameters

--- JuniorMind1/Parquet/Parquet.cs
+++ JuniorMind1/Parquet/Parquet.cs
@@ -6,9 +6,15 @@ namespace Parquet
     public class Parquet
     {
         [Fact]
-        public void Test1()
+        public void NumberOfBoardsForIntegerAreaParameters()
         {
+            int boardsNumber = Convert.ToInt32(Math.Ceiling(NumberOfParquetBoards(2, 3, 1, 2)));
+            Assert.Equal(3, boardsNumber);
+        }
 
+        double NumberOfParquetBoards(float N, float M, float A, float B)
+        {
+            return 2.1;
         }
     }
 }
