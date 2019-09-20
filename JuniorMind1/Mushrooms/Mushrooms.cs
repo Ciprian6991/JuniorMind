6d759b1daddc556b970f02ffb4f613d8dd8f3c20
A.Ciprian <ciprian.anita96@yahoo.com>
2019-09-20 10:47:50 +0300

    Calculate red mushrooms when the multiply factor is 1

--- JuniorMind1/Mushrooms/Mushrooms.cs
+++ JuniorMind1/Mushrooms/Mushrooms.cs
@@ -6,9 +6,15 @@ namespace Mushrooms
     public class Mushrooms
     {
         [Fact]
-        public void Test1()
+        public void NumberOfRedMushroomsForEquality()
         {
+            int redMushrooms = CalculateNumberOfRedMushrooms(10, 1);
+            Assert.Equal(5, redMushrooms);
+        }
 
+        int CalculateNumberOfRedMushrooms(int numberOfMushrooms, int xFactorForWhiteMushrooms)
+        {
+            return numberOfMushrooms / (xFactorForWhiteMushrooms + 1) * xFactorForWhiteMushrooms;
         }
     }
 }
