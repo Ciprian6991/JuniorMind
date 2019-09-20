a0acdd2cb2234f3790aa52f25331010ce524b12a
A.Ciprian <ciprian.anita96@yahoo.com>
2019-09-20 11:49:30 +0300

    Revert "Calculate number of boards for integer area parameters"
    This reverts commit e8038981209c12ba2959fa899b0cb716f6aa3002.

--- JuniorMind1/Parquet/Parquet.cs
+++ JuniorMind1/Parquet/Parquet.cs
@@ -6,15 +6,9 @@ namespace Parquet
     public class Parquet
     {
         [Fact]
-        public void NumberOfBoardsForIntegerAreaParameters()
+        public void Test1()
         {
-            int boardsNumber = Convert.ToInt32(Math.Ceiling(NumberOfParquetBoards(2, 3, 1, 2)));
-            Assert.Equal(3, boardsNumber);
-        }
 
-        double NumberOfParquetBoards(float N, float M, float A, float B)
-        {
-            return 2.1;
         }
     }
 }
