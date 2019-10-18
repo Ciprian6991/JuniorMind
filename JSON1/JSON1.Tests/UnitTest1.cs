101af29f71474c52c8c687b2b3ff9ec577c2a50d
A.Ciprian <ciprian.anita96@yahoo.com>
2019-10-18 09:22:20 +0300

    Add empty JSON1 and JSON1.Tests project

---
+++ JSON1/JSON1.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+using JSON1;
+
+namespace JSON1.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
