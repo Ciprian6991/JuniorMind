88e7746c097a070a10f9320796b4b587a2877792
A.Ciprian <ciprian.anita96@yahoo.com>
2019-11-01 11:28:51 +0200

    Add StudentsClass and StudentsClass.Facts

---
+++ OopStudentPosition/OopStudentPosition.Facts/StudentsClass.Facts.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace OopStudentPosition.Facts
+{
+        public class StudentsClassFacts
+        {
+            [Fact]
+            public void NullStudentNumbersValueIsInvalid()
+            {
+                Student[] sts = Array.Empty<Student>();
+                StudentsClass students = new StudentsClass(sts);
+                Assert.Equal(-1, students.SearchPositionOfStudentByName(""));
+            }
+
+            [Fact]
+            public void ShowsThePositionsForASingleStudentWithValidParameters()
+            {
+                Student st1 = new Student(9.81, "Sabin");
+                Student[] sts = new Student[1];
+                sts[0] = st1;
+                StudentsClass students = new StudentsClass(sts);
+                Assert.Equal(0, students.SearchPositionOfStudentByName("Sabin"));
+            }
+
+            [Fact]
+            public void ShowsThePositionsForASingleStudentWithInvalidParameters()
+            {
+                Student st1 = new Student(9.81, "Sabin");
+                Student[] sts = new Student[1];
+                sts[0] = st1;
+                StudentsClass students = new StudentsClass(sts);
+                Assert.Equal(-1, students.SearchPositionOfStudentByName("Zabin"));
+            }
+
+            [Fact]
+            public void ShowsThePositionsForFiveStudentsWithValidParameters()
+            {
+                Student st1 = new Student(9.81, "Sabin");
+                Student st2 = new Student(5.31, "Adrian");
+                Student st3 = new Student(9.01, "Elena");
+                Student st4 = new Student(8.43, "Costas");
+                Student st5 = new Student(7.13, "Andreea");
+                Student[] sts = { st1, st2, st3, st4, st5 };
+                StudentsClass students = new StudentsClass(sts);
+                Assert.Equal(4, students.SearchPositionOfStudentByName("Andreea"));
+            }
+
+            [Fact]
+            public void ShowsThePositionsForFiveStudentsWithInvalidParameters()
+            {
+                Student st1 = new Student(9.81, "Sabin");
+                Student st2 = new Student(5.31, "Adrian");
+                Student st3 = new Student(9.01, "Elena");
+                Student st4 = new Student(8.43, "Costas");
+                Student st5 = new Student(7.13, "Andreea");
+                Student[] sts = { st1, st2, st3, st4, st5 };
+                StudentsClass students = new StudentsClass(sts);
+                Assert.Equal(-1, students.SearchPositionOfStudentByName("Daniela"));
+            }
+        }
+}
---
+++ OopStudentPosition/OopStudentPosition/StudentsClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OopStudentPosition
+{
+    public class StudentsClass
+    {
+        readonly Student[] students;
+
+        public StudentsClass(Student[] students)
+        {
+            this.students = students;
+        }
+
+        public StudentsClass(Student student)
+        {
+            this.students[students.Length + 1] = student;
+        }
+
+        public int SearchPositionOfStudentByName(string studentName)
+        {
+            for (int i = 0; i < students.Length; i++)
+            {
+                if (students[i].HasMatchedName(studentName))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
