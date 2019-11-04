43df6d597ee712ef061b7c0d9e3c139d465c6a12
A.Ciprian <ciprian.anita96@yahoo.com>
2019-11-04 16:47:51 +0200

    Added TestForTwoClassesThatHaveDifferentNamesReturnsFalse

--- OopStudentPosition/OopStudentPosition.Facts/SchoolClass.Facts.cs
+++ OopStudentPosition/OopStudentPosition.Facts/SchoolClass.Facts.cs
@@ -20,6 +20,18 @@ namespace OopStudentPosition.Facts
         }
 
         [Fact]
+        public void TestForTwoClassesThatHaveDifferentNamesReturnsFalse()
+        {
+            double[] grades1 = { 5, 8, 9, 10 };
+            string className1 = "Math";
+            double[] grades2 = { 2, 1, 5, 7 };
+            string className2 = "Sport";
+            SchoolClass class1 = new SchoolClass(className1, grades1);
+            SchoolClass class2 = new SchoolClass(className2, grades2);
+            Assert.False(class1.HasSameName(class2));
+        }
+
+        [Fact]
         public void TestForAverageGrade()
         {
             double[] grades1 = { 8, 10 };
