14d1cb3f7370edf8f799b1280b7f98df5bccc0c1
A.Ciprian <ciprian.anita96@yahoo.com>
2019-11-11 13:35:05 +0200

    Added Range class and RangeFacts

---
+++ Lesson4Abstracting/Lesson4Abstracting.Facts/Range.Facts.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+
+namespace Lesson4Abstracting.Facts
+{
+    public class RangeFacts
+    {
+        [Fact]
+        public void TestIfMatchMethodReturnsTrue()
+        {
+            var digit = new Range('a', 'f');
+            Assert.True(digit.Match("abc"));
+        }
+    }
+}
