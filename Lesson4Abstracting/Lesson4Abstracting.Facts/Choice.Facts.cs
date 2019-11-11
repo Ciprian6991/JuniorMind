b50695947e4dea4c57cc5194bb782a998a7517c4
A.Ciprian <ciprian.anita96@yahoo.com>
2019-11-11 20:22:20 +0200

    Added Choice class and tests

---
+++ Lesson4Abstracting/Lesson4Abstracting.Facts/Choice.Facts.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+
+namespace Lesson4Abstracting.Facts
+{
+    public class ChoiceFacts
+    {
+        [Fact]
+        public void TestIfIPatternWorksForCharacterAndRAngeClasses()
+        {
+            var digitPatterns = new Choice(new Character('0'), new Range('1', '9'));
+            const string text = "00001";
+            Assert.True(digitPatterns.Match(text));
+        }
+    }
+}
---
+++ Lesson4Abstracting/Lesson4Abstracting/Choice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson4Abstracting
+{
+    public class Choice
+    {
+        readonly Character character;
+        readonly Range range;
+
+        public Choice(Character character, Range range)
+        {
+            this.character = character;
+            this.range = range;
+        }
+
+        public bool Match(string text)
+        {
+            return character.Match(text) || range.Match(text);
+        }
+    }
+}
