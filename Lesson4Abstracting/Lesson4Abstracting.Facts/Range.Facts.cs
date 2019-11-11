7f5ddc2878adde46337f8814538fcf01174c5ac7
A.Ciprian <ciprian.anita96@yahoo.com>
2019-11-11 13:37:17 +0200

    Added new RangeFacts tests

--- Lesson4Abstracting/Lesson4Abstracting.Facts/Range.Facts.cs
+++ Lesson4Abstracting/Lesson4Abstracting.Facts/Range.Facts.cs
@@ -11,5 +11,26 @@ namespace Lesson4Abstracting.Facts
             var digit = new Range('a', 'f');
             Assert.True(digit.Match("abc"));
         }
+
+        [Fact]
+        public void TestIfMatchMethodReturnsFalse()
+        {
+            var digit = new Range('a', 'f');
+            Assert.False(digit.Match("1bc"));
+        }
+
+        [Fact]
+        public void TestIfMatchMethodReturnsFalseIfNullText()
+        {
+            var digit = new Range('a', 'f');
+            Assert.False(digit.Match(""));
+        }
+
+        [Fact]
+        public void TestIfMatchMethodReturnsTrueForInverseStringOrder()
+        {
+            var digit = new Range('a', 'f');
+            Assert.True(digit.Match("cba"));
+        }
     }
 }
