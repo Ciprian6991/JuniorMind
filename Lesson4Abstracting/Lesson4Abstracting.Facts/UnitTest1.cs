281050b0bd205036f747a4eab69aa2d03056e1da
A.Ciprian <ciprian.anita96@yahoo.com>
2019-11-11 12:37:45 +0200

    Added Lesson4Abstracting and Lesson4Abstracting.Facts new projects

---
+++ Lesson4Abstracting/Lesson4Abstracting.Facts/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Lesson4Abstracting.Facts
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Lesson4Abstracting/Lesson4Abstracting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson4Abstracting
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
