08492b4bb4e3868e09a885498d2dcff9730785cd
A.Ciprian <ciprian.anita96@yahoo.com>
2019-11-11 21:33:46 +0200

    Added new tests for Choice class

--- Lesson4Abstracting/Lesson4Abstracting.Facts/Choice.Facts.cs
+++ Lesson4Abstracting/Lesson4Abstracting.Facts/Choice.Facts.cs
@@ -37,5 +37,41 @@ namespace Lesson4Abstracting.Facts
             const string text = "a0001";
             Assert.True(hex.Match(text));
         }
+
+        [Fact]
+        public void TestIfChoiceClassWorksForTwoDigits()
+        {
+            var digit = new Choice(new Character('0'), new Range('1', '9'));
+            var hex = new Choice(digit, new Choice(new Range('a', 'f'), new Range('A', 'F')));
+            const string text = "92";
+            Assert.True(hex.Match(text));
+        }
+
+        [Fact]
+        public void TestIfChoiceClassWorksForADigitAndACharacter()
+        {
+            var digit = new Choice(new Character('0'), new Range('1', '9'));
+            var hex = new Choice(digit, new Choice(new Range('a', 'f'), new Range('A', 'F')));
+            const string text = "a8";
+            Assert.True(hex.Match(text));
+        }
+
+        [Fact]
+        public void TestIfChoiceClassWorksForACapitalCharacterAndADigit()
+        {
+            var digit = new Choice(new Character('0'), new Range('1', '9'));
+            var hex = new Choice(digit, new Choice(new Range('a', 'f'), new Range('A', 'F')));
+            const string text = "B0";
+            Assert.True(hex.Match(text));
+        }
+
+        [Fact]
+        public void TestIfChoiceClassWorksForNullString()
+        {
+            var digit = new Choice(new Character('0'), new Range('1', '9'));
+            var hex = new Choice(digit, new Choice(new Range('a', 'f'), new Range('A', 'F')));
+            const string text = null;
+            Assert.False(hex.Match(text));
+        }
     }
 }
