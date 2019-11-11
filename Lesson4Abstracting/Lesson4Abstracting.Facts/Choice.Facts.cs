05a29734bff732c173255846657d05ffe2caea86
A.Ciprian <ciprian.anita96@yahoo.com>
2019-11-11 21:29:28 +0200

    Modified Choice class in order to initialize by receiving Choise classes as param + test

--- Lesson4Abstracting/Lesson4Abstracting.Facts/Choice.Facts.cs
+++ Lesson4Abstracting/Lesson4Abstracting.Facts/Choice.Facts.cs
@@ -12,5 +12,30 @@ namespace Lesson4Abstracting.Facts
             const string text = "00001";
             Assert.True(digitPatterns.Match(text));
         }
+
+        [Fact]
+        public void TestIfIPatternWorksForCharacterAndRAngeClassesReturnsFalse()
+        {
+            var digitPatterns = new Choice(new Character('0'), new Range('1', '9'));
+            const string text = "a0001";
+            Assert.False(digitPatterns.Match(text));
+        }
+
+        [Fact]
+        public void TestIfIPatternWorksForCharacterAndRAngeClassesReturnsFalseIfNullString()
+        {
+            var digitPatterns = new Choice(new Character('0'), new Range('1', '9'));
+            const string text = "";
+            Assert.False(digitPatterns.Match(text));
+        }
+
+        [Fact]
+        public void TestIfChoiceClassWorksByReceivingTwoCharacterClassesAsParameters()
+        {
+            var digit = new Choice(new Character('0'), new Range('1', '9'));
+            var hex = new Choice(digit, new Choice(new Range('a', 'f'), new Range('A', 'F')));
+            const string text = "a0001";
+            Assert.True(hex.Match(text));
+        }
     }
 }
