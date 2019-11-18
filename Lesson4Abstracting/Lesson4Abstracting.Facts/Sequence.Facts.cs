b43278e449fed5601e04a6347a825c4c66a51c95
A.Ciprian <ciprian.anita96@yahoo.com>
2019-11-18 20:20:45 +0200

    Added Sequence class and tests

---
+++ Lesson4Abstracting/Lesson4Abstracting.Facts/Sequence.Facts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Lesson4Abstracting.Facts
+{
+    public class SequenceFacts
+    {
+        [Fact]
+        public void TestIfTwoCharSequenceReturnsTrue()
+        {
+            var ab = new Sequence(new Character('a'), new Character('b'));
+            Assert.True(ab.Match("abcd").Success());
+        }
+    }
+}
---
+++ Lesson4Abstracting/Lesson4Abstracting/Sequence.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson4Abstracting
+{
+    public class Sequence : IPattern
+    {
+        readonly IPattern[] patterns;
+
+        public Sequence(params IPattern[] patterns)
+        {
+            this.patterns = patterns;
+        }
+
+        public IMatch Match(string text)
+        {
+            Match matching = new Match(text, false);
+            foreach (IPattern pattern in patterns)
+            {
+                if (pattern.Match(matching.RemainingText()).Success())
+                {
+                    matching = new Match(pattern.Match(matching.RemainingText()).RemainingText(), true);
+                }
+            }
+
+            return matching;
+        }
+    }
+}
