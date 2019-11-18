a7d27d58b119febf07b0f2655280d4411cba4692
A.Ciprian <ciprian.anita96@yahoo.com>
2019-11-18 19:46:56 +0200

    Deleted 2 unnecessary lines

--- Lesson4Abstracting/Lesson4Abstracting/Choice.cs
+++ Lesson4Abstracting/Lesson4Abstracting/Choice.cs
@@ -21,8 +21,6 @@ namespace Lesson4Abstracting
                 if (pattern.Match(matching.RemainingText()).Success())
                 {
                     matching = new Match(pattern.Match(matching.RemainingText()).RemainingText(), pattern.Match(matching.RemainingText()).Success());
-                    Console.WriteLine(matching.RemainingText());
-                    Console.WriteLine(matching.Success());
                 }
             }
 
