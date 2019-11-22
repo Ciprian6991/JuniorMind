ddb014dc8699ee491cb4490debc024becc41be19
A.Ciprian <ciprian.anita96@yahoo.com>
2019-11-22 18:02:52 +0200

    Added Any and AnyFacts classes

---
+++ Lesson4Abstracting/Lesson4Abstracting.Facts/Any.Facts.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+
+namespace Lesson4Abstracting.Facts
+{
+    public class AnyFacts
+    {
+        [Fact]
+        public void TestIfIPatternWorksForCharacterAndRAngeClasses()
+        {
+            var e = new Any("eE");
+            Assert.True(e.Match("ea").Success());
+        }
+    }
+}
---
+++ Lesson4Abstracting/Lesson4Abstracting/Any.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson4Abstracting
+{
+    public class Any
+    {
+        readonly string accepted;
+
+        public Any(string accepted)
+        {
+            this.accepted = accepted;
+        }
+
+        public IMatch Match(string text)
+        {
+                for (int i = 0; i < accepted.Length; i++)
+                {
+                    if (text[0] == accepted[i])
+                    {
+                        return new Match(text.Substring(1), true);
+                    }
+                }
+
+                return new Match(text, false);
+        }
+    }
+}
