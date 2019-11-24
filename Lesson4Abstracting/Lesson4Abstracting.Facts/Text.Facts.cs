a5096abc4dc20c422f81e2fc379816e89e69ab2e
A.Ciprian <ciprian.anita96@yahoo.com>
2019-11-24 20:44:50 +0200

    Added Text and Text.Facts classes; validates if the string is prefixed by a given string

---
+++ Lesson4Abstracting/Lesson4Abstracting.Facts/Text.Facts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Lesson4Abstracting.Facts
+{
+    public class TextFacts
+    {
+        [Fact]
+        public void TrueAsStringParameterShouldReturnTrue()
+        {
+            var truE = new Text("true");
+            Assert.True(truE.Match("true").Success());
+        }
+    }
+}
---
+++ Lesson4Abstracting/Lesson4Abstracting/Text.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson4Abstracting
+{
+    public class Text : IPattern
+    {
+        readonly string prefix;
+
+        public Text(string prefix)
+        {
+            this.prefix = prefix;
+        }
+
+        public IMatch Match(string text)
+        {
+            if (string.Equals(prefix, text, StringComparison.Ordinal))
+            {
+                return new Match("", true);
+            }
+
+            return new Match(text, false);
+        }
+    }
+}
