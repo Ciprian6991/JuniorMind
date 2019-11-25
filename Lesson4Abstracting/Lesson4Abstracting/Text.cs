5025b6eff57da2a7015edd08fb54bb918b3ccc7c
A.Ciprian <ciprian.anita96@yahoo.com>
2019-11-25 13:30:55 +0200

    Rewrite Match in Text.cs

--- Lesson4Abstracting/Lesson4Abstracting/Text.cs
+++ Lesson4Abstracting/Lesson4Abstracting/Text.cs
@@ -15,21 +15,13 @@ namespace Lesson4Abstracting
 
         public IMatch Match(string text)
         {
-            if (string.IsNullOrEmpty(prefix))
-            {
-                return new Match(text, true);
-            }
-
-            if (!string.IsNullOrEmpty(text)
-                && text.Length >= prefix.Length
-                && text.Contains(prefix, StringComparison.Ordinal))
-            {
-                return prefix.Length != text.Length
-                    ? new Match(text.Substring(text.LastIndexOf(text[prefix.Length])), true)
-                    : new Match("", true);
-            }
-
-            return new Match(text, false);
+            return string.IsNullOrEmpty(prefix)
+                ? new Match(text, true)
+                : string.IsNullOrEmpty(text)
+                ? new Match(text, false)
+                : text.StartsWith(prefix)
+                ? new Match(text.TrimStart(prefix.ToCharArray()), true)
+                : new Match(text, false);
         }
     }
 }
