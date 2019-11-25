2a0ff3b13c9ee9810f485187ae809371c9d6666c
A.Ciprian <ciprian.anita96@yahoo.com>
2019-11-25 13:03:22 +0200

    Added Many and ManyFacts

---
+++ Lesson4Abstracting/Lesson4Abstracting.Facts/Many.Facts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Lesson4Abstracting.Facts
+{
+    public class ManyFacts
+    {
+        [Fact]
+        public void AbcAsParamReturnsBcForA()
+        {
+            var a = new Many(new Character('a'));
+            Assert.Equal("bc",a.Match("abc").RemainingText());
+        }
+    }
+}
---
+++ Lesson4Abstracting/Lesson4Abstracting/Many.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson4Abstracting
+{
+    public class Many : IPattern
+    {
+        readonly IPattern pattern;
+
+        public Many(IPattern pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public IMatch Match(string text)
+        {
+            string workingString = text;
+            while (pattern.Match(workingString).Success())
+            {
+                workingString = workingString.Substring(1);
+            }
+
+            return new Match(workingString, true);
+        }
+    }
+}
