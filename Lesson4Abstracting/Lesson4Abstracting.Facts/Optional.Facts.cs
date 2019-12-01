20bae8f61f4237fdfe58982355d0a6135db778b9
A.Ciprian <ciprian.anita96@yahoo.com>
2019-12-01 16:02:40 +0200

    Added Optional and OptionalFacts classes

---
+++ Lesson4Abstracting/Lesson4Abstracting.Facts/Optional.Facts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Lesson4Abstracting.Facts
+{
+    public class OptionalFacts
+    {
+        [Fact]
+        public void AbcAsParamReturnsBcForA()
+        {
+            var a = new Optional(new Character('a'));
+            Assert.Equal("bc", a.Match("abc").RemainingText());
+        }
+    }
+}
---
+++ Lesson4Abstracting/Lesson4Abstracting/Optional.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson4Abstracting
+{
+    public class Optional
+    {
+        readonly IPattern pattern;
+
+        public Optional(IPattern pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public IMatch Match(string text)
+        {
+            string workingText = text;
+
+            if (pattern.Match(workingText).Success())
+            {
+                workingText = workingText.Substring(1);
+            }
+
+            return new Match(workingText, true);
+        }
+    }
+}
