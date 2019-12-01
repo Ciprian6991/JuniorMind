873d431adf0004e85d89c7bc32777707abd4805f
A.Ciprian <ciprian.anita96@yahoo.com>
2019-12-01 16:50:53 +0200

    Added OneOrMore and OneOrMore.Facts classes

---
+++ Lesson4Abstracting/Lesson4Abstracting.Facts/OneOrMore.Facts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Lesson4Abstracting.Facts
+{
+    public class OneOrMoreFacts
+    {
+        [Fact]
+        public void NumberAsParamReturnsEmptyStringForA()
+        {
+            var a = new OneOrMore(new Range('0', '9'));
+            Assert.Equal("", a.Match("123").RemainingText());
+        }
+    }
+}
