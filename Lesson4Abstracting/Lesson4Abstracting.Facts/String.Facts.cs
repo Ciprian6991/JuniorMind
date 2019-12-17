b07ae619ea1d9aa477258ee507b583a56ae60b49
A.Ciprian <ciprian.anita96@yahoo.com>
2019-12-17 11:06:30 +0200

    Added String and StringFacts

---
+++ Lesson4Abstracting/Lesson4Abstracting.Facts/String.Facts.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Lesson4Abstracting.Facts
+{
+    public class StringFacts
+    {
+        [Fact]
+        public void EmptyStringReturnsEmptyString()
+        {
+            var text = new String();
+            Assert.Equal("", text.Match("\"\"").RemainingText());
+        }
+
+        [Fact]
+        public void EmptyStringReturnsTrue()
+        {
+            var text = new String();
+            Assert.True(text.Match("\"\"").Success());
+        }
+    }
+}
---
+++ Lesson4Abstracting/Lesson4Abstracting/String.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson4Abstracting
+{
+    public class String
+    {
+        private readonly IPattern pattern;
+
+        public String()
+        {
+            pattern = new Sequence(new Character('"'), new Character('"'));
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
