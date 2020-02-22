15c5f34d17798bcd28514b5ccc468b86a010dea2
A.Ciprian <ciprian.anita96@yahoo.com>
2020-02-22 23:12:13 +0200

    Reading a text file

--- Lesson4Abstracting/Lesson4Abstracting/Program.cs
+++ Lesson4Abstracting/Lesson4Abstracting/Program.cs
@@ -4,9 +4,11 @@ namespace Lesson4Abstracting
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string text = System.IO.File.ReadAllText(@"C:\Users\Anita Ciprian\Desktop\afis.txt");
+            Console.WriteLine("Contents of WriteText.txt = {0}", text);
+            Console.Read();
         }
     }
 }
