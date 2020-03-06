16e22a6eb6e55b055555eba5fd7cb83d5260a772
A.Ciprian <ciprian.anita96@yahoo.com>
2020-03-06 10:41:43 +0200

    Added ListCollection<T>  + tests

---
+++ Lesson5/Lesson5.Tests/ListCollectionFacts.cs
@@ -0,0 +1,35 @@
+﻿using Xunit;
+
+namespace Lesson5.Tests
+{
+    public class ListFacts
+    {
+        [Fact]
+        public void TestListOfStringObjects()
+        {
+
+            var stringList = new ListCollection<string> { "string1", "string2" };
+
+            stringList.Add("string3");
+            stringList.Add("1");
+
+            var numerator = stringList.GetEnumerator();
+
+            for (int i = 0; i < 10; i++)
+            {
+                numerator.MoveNext();
+            }
+
+            var current = numerator.Current;
+
+            Assert.Equal("1", current);
+
+            Assert.Equal("string1", stringList[0]);
+            Assert.Equal("string2", stringList[1]);
+            Assert.Equal("string3", stringList[2]);
+            Assert.Equal("1", stringList[3]);
+            Assert.Equal(4, stringList.Count);
+
+        }
+    }
+}
---
+++ Lesson5/Lesson5/ListCollection.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson5
+{
+    public class ListCollection<T> : IEnumerable<T>
+    {
+        protected const int SizeFactor = 4;
+        private T[] array;
+
+        public ListCollection()
+        {
+            array = new T[SizeFactor];
+        }
+
+        public int Count { get; private set; }
+
+        public virtual T this[int index]
+        {
+            get => array[index];
+            set => array[index] = value;
+        }
+
+        public virtual void Add(T element)
+        {
+            CheckSize();
+            array[Count++] = element;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<T>)array).GetEnumerator();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return array[i];
+            }
+        }
+
+        private void CheckSize()
+            {
+            if (Count != array.Length)
+            {
+                return;
+            }
+
+            Array.Resize(ref array, array.Length + SizeFactor);
+        }
+    }
+}
