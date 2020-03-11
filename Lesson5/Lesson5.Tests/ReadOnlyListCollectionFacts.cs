75d9fe4883d23367354525f0e08afa08885f2ad0
A.Ciprian <ciprian.anita96@yahoo.com>
2020-03-11 16:02:18 +0200

    Rest of tests for ReadOnlyList class

--- Lesson5/Lesson5.Tests/ReadOnlyListCollectionFacts.cs
+++ Lesson5/Lesson5.Tests/ReadOnlyListCollectionFacts.cs
@@ -17,5 +17,111 @@ namespace Lesson5.Tests
 
             Assert.Equal("List is readonly!", checkGet.Message);
         }
+
+        [Fact]
+        public void Test_ExceptionSettingListIsReadOnly()
+        {
+            var intList = new ListCollection<int> { 1, 2, 3, 4, 5 };
+            var readOnly = new ReadOnlyListCollection<int>(intList);
+
+            var checkGet = Assert.Throws<NotSupportedException>(() => readOnly[0] = 1);
+
+            Assert.Equal("List is readonly!", checkGet.Message);
+        }
+
+
+        [Fact]
+        public void Test_ExceptionCountListIsReadOnly()
+        {
+            var intList = new ListCollection<int> { 1, 2, 3, 4, 5 };
+            var readOnly = new ReadOnlyListCollection<int>(intList);
+
+            Assert.Equal(5, readOnly.Count);
+        }
+
+        [Fact]
+        public void Test_ExceptionAddListIsReadOnly()
+        {
+            var intList = new ListCollection<int> { 1, 2, 3, 4, 5 };
+            var readOnly = new ReadOnlyListCollection<int>(intList);
+
+            var checkGet = Assert.Throws<NotSupportedException>(() => readOnly.Add(1));
+
+            Assert.Equal("List is readonly!", checkGet.Message);
+        }
+
+        [Fact]
+        public void Test_ExceptionClearListIsReadOnly()
+        {
+            var intList = new ListCollection<int> { 1, 2, 3, 4, 5 };
+            var readOnly = new ReadOnlyListCollection<int>(intList);
+
+            var checkGet = Assert.Throws<NotSupportedException>(() => readOnly.Clear());
+
+            Assert.Equal("List is readonly!", checkGet.Message);
+        }
+
+
+        [Fact]
+        public void Test_ExceptionContainsListIsReadOnly()
+        {
+            var intList = new ListCollection<int> { 1, 2, 3, 4, 5 };
+            var readOnly = new ReadOnlyListCollection<int>(intList);
+
+            Assert.Contains(1, readOnly);
+        }
+
+
+        [Fact]
+        public void Test_CopyListToArrayReadOnly()
+        {
+
+            var intList = new ListCollection<int> { 1, 2, 3 };
+            var readOnly = new ReadOnlyListCollection<int>(intList);
+            int[] array ={ 1, 2 };
+
+            var checkGet = Assert.Throws<NotSupportedException>(() => readOnly.CopyTo(array, 3));
+
+            Assert.Equal("List is readonly!", checkGet.Message);
+        }
+
+        [Fact]
+        public void Test_ExceptionIndexOfListIsReadOnly()
+        {
+            var intList = new ListCollection<int> { 1, 2, 3, 4, 5 };
+            var readOnly = new ReadOnlyListCollection<int>(intList);
+
+            Assert.Equal(2, readOnly.IndexOf(3));
+        }
+
+        [Fact]
+        public void Test_ExceptionInsertListIsReadOnly()
+        {
+            var intList = new ListCollection<int> { 1, 2, 3, 4, 5 };
+            var readOnly = new ReadOnlyListCollection<int>(intList);
+
+            var checkGet = Assert.Throws<NotSupportedException>(() => readOnly.Insert(1,2));
+
+            Assert.Equal("List is readonly!", checkGet.Message);
+        }
+
+
+        [Fact]
+        public void Test_ExceptionEnumeratorListIsReadOnly()
+        {
+            var intList = new ListCollection<int> { 1, 2, 3, 4, 5 };
+            var readOnly = new ReadOnlyListCollection<int>(intList);
+
+            var checkGet = Assert.Throws<NotSupportedException>(() => readOnly.RemoveAt(2));
+
+            var enumerator = readOnly.GetEnumerator();
+            enumerator.MoveNext();
+            enumerator.MoveNext();
+            enumerator.MoveNext();
+            enumerator.MoveNext();
+            enumerator.MoveNext();
+
+            Assert.Equal(5, enumerator.Current);
+        }
     }
 }
