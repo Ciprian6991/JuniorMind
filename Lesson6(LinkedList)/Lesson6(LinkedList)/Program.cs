a1f39a72fb04d31a4d64ff6ee2271626b274ac76
A.Ciprian <ciprian.anita96@yahoo.com>
2020-03-13 11:39:28 +0200

    Added new Project LinkedList

---
+++ Lesson6(LinkedList)/Lesson6(LinkedList)/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lesson6LinkedList
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            Console.Read();
+        }
+    }
+}
