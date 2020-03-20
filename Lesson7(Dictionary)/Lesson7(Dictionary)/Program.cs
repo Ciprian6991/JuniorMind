c21c750ebe4999160cda22eecf9ec9135bb11df1
A.Ciprian <ciprian.anita96@yahoo.com>
2020-03-20 23:00:57 +0200

    Added New Project; Empty Dictionary.cs + DictionaryFacts.cs

---
+++ Lesson7(Dictionary)/Lesson7(Dictionary)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson7Dictionary
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Lesson7(Dictionary)/Lesson7Dictionary.Tests/DictionaryFacts.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+using Lesson7Dictionary;
+
+namespace Lesson7Dictionary.Tests
+{
+    public class DictionaryFacts
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
