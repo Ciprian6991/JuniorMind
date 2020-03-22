25cd42cb35a003d5bf5512d5890db11f4e7c1a09
A.Ciprian <ciprian.anita96@yahoo.com>
2020-03-22 23:59:14 +0200

    Implemented some of IDictionary<TKey, TValue> + test for one add

--- Lesson7(Dictionary)/Lesson7(Dictionary)/Dictionary.cs
+++ Lesson7(Dictionary)/Lesson7(Dictionary)/Dictionary.cs
@@ -1,10 +1,155 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Lesson7Dictionary
 {
-    public class Dictionary
+    public class Dictionary<TKey, TValue> : IDictionary<TKey, TValue>
     {
+        private readonly int[] buckets;
+        private readonly Element[] elements;
+
+        public Dictionary(int size = 5)
+        {
+            buckets = new int[size];
+            Array.Fill(buckets, -1);
+
+            elements = new Element[size];
+
+            Count = 0;
+        }
+
+        public ICollection<TKey> Keys
+        {
+            get
+            {
+                var list = new List<TKey>();
+
+                foreach (var element in this)
+                {
+                    list.Add(element.Key);
+                }
+
+                return list;
+            }
+        }
+
+        public ICollection<TValue> Values
+        {
+            get
+            {
+                var list = new List<TValue>();
+
+                foreach (var element in this)
+                {
+                    list.Add(element.Value);
+                }
+
+                return list;
+            }
+        }
+
+        public int Count { get; private set; }
+
+        public bool IsReadOnly => false;
+
+        public TValue this[TKey key] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void Add(TKey key, TValue value)
+        {
+            AddElement(ref elements[Count], key, value);
+
+            int bucketIndex = Math.Abs(key.GetHashCode()) % buckets.Length;
+            if (buckets[bucketIndex] != -1)
+            {
+                elements[Count].Next = buckets[bucketIndex];
+            }
+
+            buckets[bucketIndex] = Count;
+            Count++;
+        }
+
+        public void Add(KeyValuePair<TKey, TValue> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(KeyValuePair<TKey, TValue> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            for (int i = buckets[Math.Abs(key.GetHashCode()) % buckets.Length]; i != -1; i = elements[i].Next)
+            {
+                if (elements[i].Key.Equals(key))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return GetBucket(elements[i]);
+            }
+        }
+
+        public bool Remove(TKey key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(KeyValuePair<TKey, TValue> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddElement(ref Element element, TKey key, TValue value)
+        {
+            element.Key = key;
+            element.Value = value;
+            element.Next = -1;
+        }
+
+        private KeyValuePair<TKey, TValue> GetBucket(Element element)
+        {
+            return KeyValuePair.Create(element.Key, element.Value);
+        }
+
+        struct Element
+        {
+            public TKey Key { get; set; }
+
+            public TValue Value { get; set; }
+
+            public int Next { get; set; }
+        }
     }
 }
--- Lesson7(Dictionary)/Lesson7Dictionary.Tests/DictionaryFacts.cs
+++ Lesson7(Dictionary)/Lesson7Dictionary.Tests/DictionaryFacts.cs
@@ -7,8 +7,13 @@ namespace Lesson7Dictionary.Tests
     public class DictionaryFacts
     {
         [Fact]
-        public void Test1()
+        public void Test_AddOneElement()
         {
+            var dic = new Dictionary<int, string>(5)
+            {
+                { 1, "a" }
+            };
+            Assert.True(dic.ContainsKey(1));
         }
     }
 }
