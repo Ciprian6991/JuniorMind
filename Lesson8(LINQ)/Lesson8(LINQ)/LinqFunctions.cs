230f4ac91fa46227051f75d9fc6303279edbc494
A.Ciprian <ciprian.anita96@yahoo.com>
2020-04-06 23:29:03 +0300

    Added All<TSource> and test for true

---
+++ Lesson8(LINQ)/Lesson8(LINQ)/LinqFunctions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson8LINQ
+{
+    public static class LinqFunctions
+    {
+        public static bool All<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            if (source == null)
+            {
+                return false;
+            }
+
+            foreach (var element in source)
+            {
+                if (!predicate(element))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Lesson8(LINQ)/Lesson8LINQ.Tests/LinqFunctionsFacts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Lesson8LINQ.Tests
+{
+    public class LinqFunctionsFacts
+    {
+        [Fact]
+        public void TestAllWhenTrue()
+        {
+            var array = new int[] { 2, 4, 6 };
+
+            Func<int, bool> myFunc = (x) => { return x % 2 == 0; };
+
+            Assert.True(LinqFunctions.All(array, p => myFunc(p)));
+        }
+    }
+}
