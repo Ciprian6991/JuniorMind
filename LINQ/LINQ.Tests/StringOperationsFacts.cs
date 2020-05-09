79b9e9bd8b0685c56bdb5f1062d9d0036b039ac5
A.Ciprian <ciprian.anita96@yahoo.com>
2020-05-09 17:52:09 +0300

    Added StringOperations.cs + test class // added .CountVocals()

---
+++ LINQ/LINQ.Tests/StringOperationsFacts.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LINQ.Tests
+{
+    public class StringOperationsFacts
+    {
+        [Fact]
+        public void Test_CountVocals()
+        {
+            int count = StringOperations.CountVocals("Four Words Seven Vocals");
+
+            Assert.Equal(7, count);
+        }
+    }
+}
---
+++ LINQ/LINQ/StringOperations.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LINQ;
+
+namespace LINQ
+{
+    public class StringOperations
+    {
+        public static int CountVocals(string word)
+        {
+            return word.Aggregate(0, (vocalsNumber, curentChar) => "aeiouAEIOU".Contains(curentChar) ? vocalsNumber + 1 : vocalsNumber);
+        }
+    }
+}
