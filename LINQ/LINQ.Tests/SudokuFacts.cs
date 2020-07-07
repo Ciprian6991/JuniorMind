458330201e7f4ab2bfac797a72132cf8b09be6a3
A.Ciprian <ciprian.anita96@yahoo.com>
2020-07-07 11:58:12 +0300

    Added Sudoku.cs  + .CheckLine() + test

---
+++ LINQ/LINQ.Tests/SudokuFacts.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LINQ.Tests
+{
+    public class SudokuFacts
+    {
+        [Fact]
+        public void CheckSudoku_Check_Line_True()
+        {
+
+            var line = new int[]
+            {
+                1, 2, 3, 4, 5, 6, 7, 8, 9
+            };
+
+            var result = Sudoku.CheckLine(line);
+            
+            Assert.True(result);
+        }
+    }
+}
---
+++ LINQ/LINQ/Sudoku.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LINQ
+{
+    public class Sudoku
+    {
+        private readonly int[][] square;
+
+        public Sudoku(int[][] square)
+        {
+            this.square = square;
+        }
+
+        public static bool CheckLine(IEnumerable<int> line)
+        {
+            return line.All(digit => Enumerable.Range(1, line.Count()).Contains(digit));
+        }
+
+        private IEnumerable<IEnumerable<int>> GetLines()
+        {
+            return square.Select(x => x);
+        }
+    }
+}
